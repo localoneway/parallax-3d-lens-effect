c0792d207781c046691359e5d1517fdb37d56145
localoneway <andrewdelman@gmail.com>
2024-05-29 16:36:31 -0700

    looking good

--- src/scripts/rain.js
+++ src/scripts/rain.js
@@ -9,40 +9,43 @@ const random = (min, max) => Math.floor(Math.random() * max) + min;
  */
 class RainDrop {
   /**
-   * @param { CanvasRenderingContext2D} context
+   * @param {CanvasRenderingContext2D} context
    * @param {Number} x
    * @param {Number} y
    * @param {Number} size
    * @param {Number} thickness
-   * @param {Number} velocity
+   * @param {Number} velocityX
+   * @param {Number} velocityY
    * @param {Number} opacity
    */
-  constructor(context, { x, y, size, thickness, velocity, opacity }) {
+  constructor(context, { x, y, size, thickness, velocityX, velocityY, opacity }) {
     this.context = context;
     this.x = x;
     this.y = y;
-    this.endY = size;
+    this.size = size;
     this.thickness = thickness;
-    this.velocity = velocity;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
     this.opacity = opacity;
   }
 
   draw() {
-    const { context, x, y, endY, thickness, opacity } = this;
+    const { context, x, y, size, thickness, opacity } = this;
     context.beginPath();
     context.moveTo(x, y);
-    context.lineTo(x, y - endY);
+    context.lineTo(x + this.velocityX, y + this.velocityY);
     context.lineWidth = thickness;
     context.strokeStyle = 'rgba(255, 255, 255, ' + opacity + ')';
     context.stroke();
   }
 
   update() {
-    const canvasBottom = window.innerHeight + 100;
-    this.y = this.y >= canvasBottom ? this.endY - 100 : this.y + this.velocity;
+    this.x += this.velocityX;
+    this.y += this.velocityY;
     this.draw();
   }
 }
+
 
 /**
  * The 'rain effect' in the background.
@@ -54,27 +57,33 @@ class Rain {
    * @param {Range} size The range of drop size.
    * @param {Range} thickness The range of drop thickness.
    * @param {Range} speed The range of drop speed.
-   * @param {Number} amount Amount of drops.
+   * @param {Number} amount Initial amount of drops.
+   * @param {Number} interval Time interval for adding new drops in milliseconds.
    */
-  constructor({ size = [2, 10], thickness = [1, 2], speed = [2, 20], amount = 140 } = {}) {
+  constructor({ size = [2, 10], thickness = [1, 2], speed = [2, 20], amount = 140, interval = 100 } = {}) {
     const [sizeMin, sizeMax] = size;
     const [speedMin, speedMax] = speed;
     const [thicknessMin, thicknessMax] = thickness;
 
+    this.sizeRange = size;
+    this.thicknessRange = thickness;
+    this.speedRange = speed;
+    this.interval = interval;
+
     this.#initCanvas();
 
+    this.centerX = window.innerWidth / 2;
+    this.centerY = window.innerHeight / 2;
+
+    // Initial blast from the center
     for (let i = 0; i < amount; i++) {
-      this.drops.push(
-        new RainDrop(this.context, {
-          x: random(1, window.innerWidth),
-          y: Math.random() * -500,
-          size: random(sizeMin, sizeMax),
-          thickness: random(thicknessMin, thicknessMax - 1) + random(1, 9) / 10,
-          velocity: random(speedMin, speedMax),
-          opacity: Math.random() * 0.55,
-        })
-      );
+      this.addDropFromCenter();
     }
+
+    // Add new drops at regular intervals
+    setInterval(() => {
+      this.addRandomDrop();
+    }, interval);
   }
 
   #initCanvas() {
@@ -83,6 +92,54 @@ class Rain {
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+  }
+
+  addDropFromCenter() {
+    const [sizeMin, sizeMax] = this.sizeRange;
+    const [speedMin, speedMax] = this.speedRange;
+    const [thicknessMin, thicknessMax] = this.thicknessRange;
+
+    const angle = Math.random() * 2 * Math.PI;
+    const speed = random(speedMin, speedMax);
+    const velocityX = speed * Math.cos(angle);
+    const velocityY = speed * Math.sin(angle);
+
+    this.drops.push(
+      new RainDrop(this.context, {
+        x: this.centerX,
+        y: this.centerY,
+        size: random(sizeMin, sizeMax),
+        thickness: random(thicknessMin, thicknessMax - 1) + random(1, 9) / 10,
+        velocityX: velocityX,
+        velocityY: velocityY,
+        opacity: Math.random() * 0.55,
+      })
+    );
+  }
+
+  addRandomDrop() {
+    const [sizeMin, sizeMax] = this.sizeRange;
+    const [speedMin, speedMax] = this.speedRange;
+    const [thicknessMin, thicknessMax] = this.thicknessRange;
+
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    const angle = Math.random() * 2 * Math.PI;
+    const speed = random(speedMin, speedMax);
+    const velocityX = speed * Math.cos(angle);
+    const velocityY = speed * Math.sin(angle);
+
+    this.drops.push(
+      new RainDrop(this.context, {
+        x: x,
+        y: y,
+        size: random(sizeMin, sizeMax),
+        thickness: random(thicknessMin, thicknessMax - 1) + random(1, 9) / 10,
+        velocityX: velocityX,
+        velocityY: velocityY,
+        opacity: Math.random() * 0.35,
+      })
+    );
   }
 
   animate() {
